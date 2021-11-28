ed7058c2bc3d22d05689495d4c2a166ee57cf041
Aashish Sharma <aashishsharma@Aashishs-MBP.attlocal.net>
2021-11-27 22:24:03 -0600

    dom manuplation

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-//let apiQoutes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQoutes = [];
 
 //Show new quote
 function newQuote(){
     //pick a random quotes
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote)
+    const quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // Get quotes from api
-// async function getQuotes(){
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try{
-//         const response = await fetch(apiUrl);
-//         apiQoutes = await response.json();
-//         newQuote();
-//     }catch(errorr){
-//         //catch error
-//     }
+async function getQuotes(){
+    const apiUrl = 'https://type.fit/api/quotes';
+    try{
+        const response = await fetch(apiUrl);
+        apiQoutes = await response.json();
+        newQuote();
+    }catch(errorr){
+        //catch error
+    }
     
-// }
-// getQuotes();
-    newQuote();
+}
+getQuotes();
