9738e25338004e2cd3a4bf7fa6ca4b66d91b38e6
Aashish Sharma <aashishsharma@Aashishs-MBP.attlocal.net>
2021-11-27 19:21:04 -0600

    loading quotes from local file

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let apiQoutes = [];
+//let apiQoutes = [];
 
 //Show new quote
 function newQuote(){
     //pick a random quotes
-    const quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length)];
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     console.log(quote)
 }
 
 // Get quotes from api
-async function getQuotes(){
-    const apiUrl = 'https://type.fit/api/quotes';
-    try{
-        const response = await fetch(apiUrl);
-        apiQoutes = await response.json();
-        newQuote();
-    }catch(errorr){
-        //catch error
-    }
+// async function getQuotes(){
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try{
+//         const response = await fetch(apiUrl);
+//         apiQoutes = await response.json();
+//         newQuote();
+//     }catch(errorr){
+//         //catch error
+//     }
     
-}
-getQuotes();
+// }
+// getQuotes();
+    newQuote();
